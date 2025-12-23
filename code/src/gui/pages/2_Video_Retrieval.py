2c5a6913989c2df8c3becf056534d48a56c11b38
Petrus Nicolas Manurung <petrusnmanurung@gmail.com>
2025-12-23 22:23:53 +0700

    retriever : add knn search

--- code/src/gui/pages/2_Video_Retrieval.py
+++ code/src/gui/pages/2_Video_Retrieval.py
@@ -25,20 +25,19 @@ base_dir = os.path.join(Path(__file__).resolve().parents[4])
 
 retriever = Retriever(get_db(), base_dir) 
 
-def image_caption(fn, size, w, h, ch, ext):
+def video_caption(fn, size, dur, fps, ext):
     caption =f"""
     File name= {fn}\n
     Size= {size/(1024)} KB\n
-    Width= {w} px\n
-    Height= {h} px\n
-    N_channels= {ch}\n
+    Duration= {dur} px\n
+    FPS= {fps} px\n
     File extension= {ext}
     """
     return caption
 
 # UI
 
-st.title("Image Retrieval System")
+st.title("Video Retrieval System")
 
 uploaded = st.file_uploader(
     "Upload Image",
@@ -56,35 +55,35 @@ with st.popover("Advanced Query"):
     if search_method == None:
         st.warning("Please select a method")
 
-    max_size = st.slider("Image Size (KB)", 0, 4096, 1024)
-    min_w, max_w = st.slider("Image Width", 0, 4096, (0, 2048))
-    min_h, max_h = st.slider("Image Height", 0, 4096, (0, 2048))
+    max_size = st.slider("Video Size (KB)", 0, 4096, 1024)
+    min_fps, max_fps = st.slider("Video FPS", 0, 75, (0, 60))
+    min_dur, max_dur = st.slider("Video Duration", 0, 300, (0, 60))
     ext = st.multiselect(
-        "Image Extension",
-        ["jpg", "png", "bmp", "jpeg"],
-        default=["jpg", "png", "bmp", "jpeg"])
+        "Video Extension",
+        ["avi", "mp4", "mkv"],
+        default=["avi", "mp4", "mkv"])
 
     top_k = st.slider("Top K", 1, 10, 5)
 
-query_img = None
+query= None
 if uploaded:
     file_bytes = np.asarray(bytearray(uploaded.read()), dtype=np.uint8)
-    query_img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    query = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
     st.subheader("Query")
-    st.image(cv2.cvtColor(query_img, cv2.COLOR_BGR2RGB), width=300)
+    st.image(cv2.cvtColor(query, cv2.COLOR_BGR2RGB), width=300)
 
 if st.button("Search"):
     start_time = time.perf_counter()
 
-    filtered_img = retriever.image_metadata_filter(max_size*1024, min_w, max_w, min_h, max_h, ext)
+    filtered_img = retriever.video_metadata_filter(max_size*1024, min_fps, max_fps, min_dur, max_dur, ext)
     filtered_img = [row[0] for row in filtered_img]
 
-    if query_img is None:
+    if query is None:
         results = [(id, 0.) for id in filtered_img]
         results = results[:top_k]
     else:
-        results = retriever.image_similarity_search(query_img, search_method=search_method, top_k=top_k)
+        results = retriever.image_similarity_search(query, search_method=search_method, top_k=top_k)
         results = [(id, dist) for id, dist in results if id in filtered_img]
         results = results
     
@@ -112,6 +111,6 @@ if st.button("Search"):
                 )
                 with col.expander("Metadata"):
                     st.write(
-                        image_caption( *metadata)
+                        video_caption( *metadata)
                         )
                 
