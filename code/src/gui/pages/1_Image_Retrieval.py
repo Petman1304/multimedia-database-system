7b7ef18d3cefc12e60c6e7cf0d0891633bd9b0d1
Petrus Nicolas Manurung <petrusnmanurung@gmail.com>
2025-12-23 19:27:25 +0700

    retriever : retrieve by metadata

--- code/src/gui/pages/1_Image_Retrieval.py
+++ code/src/gui/pages/1_Image_Retrieval.py
@@ -57,8 +57,17 @@ with st.popover("Advanced Query"):
     if search_method == None:
         st.warning("Please select a method")
 
+    max_size = st.slider("Image Size (MB)", 0, 5, 2)
+    min_w, max_w = st.slider("Image Width", 0, 4096, (0, 2048))
+    min_h, max_h = st.slider("Image Height", 0, 4096, (0, 2048))
+    ext = st.multiselect(
+        "Image Extension",
+        ["jpg", "png", "bmp", "jpeg"],
+        default=["jpg", "png", "bmp", "jpeg"])
+
     top_k = st.slider("Top K", 1, 10, 5)
 
+query_img = None
 if uploaded:
     file_bytes = np.asarray(bytearray(uploaded.read()), dtype=np.uint8)
     query_img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
@@ -66,34 +75,43 @@ if uploaded:
     st.subheader("Query")
     st.image(cv2.cvtColor(query_img, cv2.COLOR_BGR2RGB), width=300)
 
-    if st.button("Search"):
-        start_time = time.perf_counter()
+if st.button("Search"):
+    start_time = time.perf_counter()
 
-        results = retriever.image_similarity_search(query_img, search_method=search_method, top_k=top_k)
-        results = retriever.fetch_image_from_db(results)
+    filtered_img = retriever.image_metadata_filter(max_size, min_w, max_w, min_h, max_h, ext)
 
-        end_time = time.perf_counter()
-        q_time = end_time - start_time
+    if query_img == None:
+        results = [(id, 0.) for id in filtered_img]
+        results = results[:top_k]
+    else:
+        results = retriever.image_similarity_search(query_img, search_method=search_method)
+        results = [(id, dist) for id, dist in results if id in filtered_img]
+        results = results[:top_k]
+    
+    results = retriever.fetch_image_from_db(results)
 
-        st.subheader("Results")
-        st.write(f"Query time : {q_time:.4f} s")
+    end_time = time.perf_counter()
+    q_time = end_time - start_time
 
-        if len(results) == 0:
-            st.write("No images match your query.")
-        else:
-            n_cols = 3 
+    st.subheader("Results")
+    st.write(f"Query time : {q_time:.4f} s")
 
-            for i in range(0, len(results), n_cols):
-                cols = st.columns(n_cols)
-                for col, (id, dist, img, metadata, label) in zip(cols, results[i:i+n_cols]):
-                    col.image(
-                        img,
-                        caption=f"Similarity Score= {dist:.4f}",
-                        channels="BGR",
-                        width="stretch"
-                    )
-                    with col.expander("Metadata"):
-                        st.write(
-                            image_caption( *metadata)
-                            )
+    if len(results) == 0:
+        st.write("No images match your query.")
+    else:
+        n_cols = 3 
+
+        for i in range(0, len(results), n_cols):
+            cols = st.columns(n_cols)
+            for col, (id, dist, img, metadata, label) in zip(cols, results[i:i+n_cols]):
+                col.image(
+                    img,
+                    caption=f"Similarity Score= {dist:.4f}",
+                    channels="BGR",
+                    width="stretch"
+                )
+                with col.expander("Metadata"):
+                    st.write(
+                        image_caption( *metadata)
+                        )
                 
